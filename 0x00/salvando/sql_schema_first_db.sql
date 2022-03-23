2a07c2732c079a7c023d5518e34329810020b17e
johnnyalvesx <alvesjohnny@outlook.com>
2022-03-23 18:24:55 -0300

    Java com Banco de Dados - Introdução - Task 4

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,14 @@
+-- TABLE
+CREATE TABLE Cliente (
+id int primary key not null, 
+nome varchar (80) not null,
+email varchar (40) not null,
+telefone int
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
